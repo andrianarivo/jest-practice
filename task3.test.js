321c3e8e36c0e62e75714535c5f5cfadac9b34b9
andrianarivo <andrianarivo.david@gmail.com>
2023-05-09 15:03:15 +0300

    build subtract, multiply, divide

--- task3.test.js
+++ task3.test.js
@@ -26,3 +26,84 @@ describe('addition', () => {
     expect(result).toBe(0);
   });
 });
+
+describe('subtract', () => {
+  it('positive numbers', () => {
+    const a = 1;
+    const b = 1;
+
+    const result = calculator.subtract(a, b);
+
+    expect(result).toBe(0);
+  });
+  it('negative numbers', () => {
+    const a = -1;
+    const b = -1;
+
+    const result = calculator.subtract(a, b);
+
+    expect(result).toBe(0);
+  });
+  it('mixed numbers', () => {
+    const a = 1;
+    const b = -1;
+
+    const result = calculator.subtract(a, b);
+
+    expect(result).toBe(2);
+  });
+});
+
+describe('multiply', () => {
+  it('positive numbers', () => {
+    const a = 1;
+    const b = 1;
+
+    const result = calculator.multiply(a, b);
+
+    expect(result).toBe(1);
+  });
+  it('negative numbers', () => {
+    const a = -1;
+    const b = -1;
+
+    const result = calculator.multiply(a, b);
+
+    expect(result).toBe(1);
+  });
+  it('mixed numbers', () => {
+    const a = 1;
+    const b = -1;
+
+    const result = calculator.multiply(a, b);
+
+    expect(result).toBe(-1);
+  });
+});
+
+describe('divide"', () => {
+  it('positive numbers', () => {
+    const a = 1;
+    const b = 1;
+
+    const result = calculator.divide(a, b);
+
+    expect(result).toBe(1);
+  });
+  it('negative numbers', () => {
+    const a = -1;
+    const b = -1;
+
+    const result = calculator.divide(a, b);
+
+    expect(result).toBe(1);
+  });
+  it('mixed numbers', () => {
+    const a = 1;
+    const b = -1;
+
+    const result = calculator.divide(a, b);
+
+    expect(result).toBe(-1);
+  });
+});
