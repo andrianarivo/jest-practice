69de7262898e70ffd12bf31a6266b81834857c38
andrianarivo <andrianarivo.david@gmail.com>
2023-05-09 14:15:48 +0300

    update task1 to use AAA

--- task1.test.js
+++ task1.test.js
@@ -1,16 +1,28 @@
 const stringLength = require('./task1');
 
 it('Length is of string', () => {
+  // Arrange
   const var1 = '1234';
-  expect(stringLength(var1)).toBe(4);
+
+  // Act
+  const result = stringLength(var1);
+
+  // Assert
+  expect(result).toBe(4);
 });
 
 it('Length is more than one', () => {
+  // Arrange
   const var1 = '';
+
+  // Assert
   expect(() => stringLength(var1)).toThrow(/too small/);
 });
 
 it('Length is less than ten', () => {
+  // Arrange
   const var1 = '0123456789a';
+
+  // Assert
   expect(() => stringLength(var1)).toThrow(/too long/);
 });
